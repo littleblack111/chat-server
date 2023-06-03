3b6b297fe822fe26a0601a181eb1fb84bd2f0b68
littleblack111 <jerrylam110209@gmail.com>
2023-06-04 01:08:07 +0800

    Implemented blocking swear/curse words and removed invalid/inapropriate names

--- main.py
+++ main.py
@@ -5,34 +5,70 @@ import threading
 HOST = '0.0.0.0'  # listen on both loopback and local IP address
 PORT = 8000
 
-VERSOIN = "1.0"
+VERSOIN = "1.1" # Implemented blocking swear/curse words and removed invalid/inapropriate names
 
+swearList = ['fuck', 'shit', 'bitch', 'ass', 'nigg']
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server_socket.bind((HOST, PORT))
 print(f"Server binded on {HOST} with port {PORT}")
 server_socket.listen()
 
 clients = []
+names = []
 
 def broadcast(message):
-    with open("../logs/chat.log", "a") as log_file:
-        log_file.write(message.decode())
+    #with open("../logs/chat.log", "a") as log_file:
+    #    log_file.write(message.decode())
     for client in clients:
         client.send(message)
+    with open("chat.log", "a") as log_file:
+        log_file.write(message.decode())
+
 
 def handle_client(client_socket, client_address):
+    print(f'New connection from {client_address}')
     if client_address[0] in [c.getpeername()[0] for c in clients]:
-        #print(f'{client_address} is already connected')
+        print(f'{client_address} is attempting to connect twice')
         client_socket.send("Your already connected\n".encode())
         client_socket.close()
         return
     clients.append(client_socket)
     client_socket.send("Name: ".encode())
-    name = client_socket.recv(1024).decode().replace("\n", "")
-    print(f'New connection from {client_address}')
+    try:
+        name = client_socket.recv(1024).decode().replace("\n", "")
+        if name:
+            if name == "" or " " in name or SwearOrNot(name):
+                for c in clients:
+                    c.send("Your name have problems\n".encode())
+                    clients.remove(c)
+                    c.close()
+                    return
+            for n in names:
+                if n == name: # need test
+                    for c in clients:
+                        snames = " ".join(names)
+                        c.send(f"Username taken, use an unused name, current names: {snames}\n".encode())
+                        clients.remove(c)
+                        c.close()
+                    return
+        else:
+            raise Exception('Client disconnected without registering')
+    except Exception as e:
+        print(e)
+        clients.remove(client_socket)
+        print(f'{client_address} has left the chat without registering')
+        #broadcast(f'{client_address} has left the chat\n'.encode())
+        broadcast(f'{client_address} has left the chat without even registering\n'.encode())
+        client_socket.close()
+        return
+
+    names.append(name)
     #broadcast(f'{client_address} has joined the chat\n'.encode())
     broadcast(f'{name} has joined the chat\n'.encode())
-    with open("../logs/chat.log", "r") as log_file:
+    #with open("../logs/chat.log", "r") as log_file:
+    #    log_contents = log_file.read()
+    #    client_socket.send(log_contents.encode())
+    with open("chat.log", "r") as log_file:
         log_contents = log_file.read()
         client_socket.send(log_contents.encode())
 
@@ -68,7 +104,10 @@ def handle_client(client_socket, client_address):
 
                 if message.decode() != "":
                     #broadcast(f'{client_address}: {message.decode()}'.encode())
-                    broadcast(f'{name}: {message.decode()}'.encode())
+                    if SwearOrNot(message.decode()):
+                        broadcast(f'{name}: {len(message.decode())*"#"}\n'.encode())
+                    else:
+                        broadcast(f'{name}: {message.decode()}'.encode())
                     print(f'{client_address}({name}): {message.decode()}')
             else:
                 raise Exception('Client disconnected')
@@ -80,6 +119,13 @@ def handle_client(client_socket, client_address):
             broadcast(f'{name} has left the chat\n'.encode())
             client_socket.close()
             return
+
+def SwearOrNot(var):
+    if any(word in var for word in swearList):
+        return True
+    else:
+        return False
+
 
 def accept_connections():
     while True:
@@ -94,6 +140,8 @@ def accept_connections():
 
 try:
     if __name__ == '__main__':
+        with open("chat.log", "w") as log_file:
+            log_file.truncate(0)
         try:
             server_thread = threading.Thread(target=accept_connections)
             server_thread.start()
