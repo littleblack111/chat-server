076fd3d441a29c862dbc94f3f7621608a0453d09
littleblack111 <littleblack11111@gmail.com>
2025-03-04 23:29:39 +0800

    refactor!: python -> c++

---
+++ src/session.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "FileDescriptor.hpp"
+#include <string>
+
+class CSession {
+  public:
+  struct SRecvData {
+    char data[1024] = {0};
+    ssize_t dataSize = sizeof(data);
+    bool good = true;
+  };
+	CSession(Hyprutils::OS::CFileDescriptor m_sockfd);
+	~CSession();
+  std::string getName() const;
+  bool isValid() const;
+  SRecvData read();
+  SRecvData read(std::string &msg);
+  bool write(const std::string &msg) const;
+  void run();
+
+  private:
+	Hyprutils::OS::CFileDescriptor m_sockfd;
+  std::string m_name;
+  void recvManager();
+  bool registerSession();
+void onConnect() const;
+void onDisconnect() const;
+void onErrno() const;
+void onRecv(SRecvData &data) const;
+  friend class CSessionManager;
+};
---
+++ src/sessionManager.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "session.hpp"
+#include <memory>
+#include <thread>
+#include <vector>
+
+class CSessionManager {
+public:
+  CSessionManager();
+  ~CSessionManager();
+  void enterLoop();
+  void newSession(Hyprutils::OS::CFileDescriptor sockfd);
+	void broadcast(const std::string &message) const;
+	void kick(const CSession *session) const;
+	void addSession(const CSession &session);
+private:
+  std::vector<std::pair<std::jthread, std::shared_ptr<CSession>>>		   m_vSessions;
+};
+inline std::unique_ptr<CSessionManager> g_pSessionManager;
