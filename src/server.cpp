2b1d5046ca883060c27daf06d6b3bc2a8342242e
littleblack111 <littleblack11111@gmail.com>
2025-03-04 23:41:06 +0800

    cleanup: random comments

--- src/server.cpp
+++ src/server.cpp
@@ -17,20 +17,7 @@ CServer::CServer(uint16_t port)
 	m_dListen			   = listen(m_sockfd.get(), 5);
 	 m_dBind < 0 || m_dListen < 0 ? throw std::runtime_error("Failed to bind or listen") : std::println("Server started on port: {}", port);
 
-	// char buffer[1024] = {0};
-	// int clientSocket = accept(m_sockfd.get(), nullptr, nullptr);
-	// recv(clientSocket, buffer, sizeof(buffer), 0);
-	// std::println("Message from client: {}", buffer);
 };
-
-// void CServer::run() {
-// 	while (true) {
-// 		char buffer[1024] = {0};
-// 		int	 clientSocket = accept(m_sockfd.get(), nullptr, nullptr);
-// 		recv(clientSocket, buffer, sizeof(buffer), 0);
-// 		std::println("Message from client: {}", buffer);
-// 	}
-// }
 
 CServer::~CServer() {
 	m_sockfd.reset();
