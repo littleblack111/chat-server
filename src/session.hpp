ea8fceb2a84150de2dd98b301901e744228f1acf
littleblack111 <littleblack11111@gmail.com>
2025-03-11 19:09:54 +0800

    bump: compile_commands

--- src/session.hpp
+++ src/session.hpp
@@ -19,18 +19,17 @@ class CSession {
 	};
 
 	std::string getName();
-	void setSelf(std::pair<std::jthread, std::shared_ptr<CSession>> *self);
+	void		setSelf(std::pair<std::jthread, std::shared_ptr<CSession>> *self);
 
-	SRecvData	read();
-	SRecvData	read(const std::string &msg);
-	bool		write(const std::string &msg, eFormatType type);
+	SRecvData read();
+	SRecvData read(const std::string &msg);
+	bool	  write(const std::string &msg, eFormatType type);
 	template <typename... Args>
 	bool write(eFormatType type, std::format_string<Args...> fmt, Args &&...args);
 
 	void run();
 
-	bool		isValid();
-
+	bool isValid();
 
   private:
 	enum eEventType : std::uint8_t {
@@ -39,22 +38,22 @@ class CSession {
 	};
 
 	std::pair<std::jthread, std::shared_ptr<CSession>> *self;
-	Hyprutils::OS::CFileDescriptor m_sockfd;
-  sockaddr_in           m_addr;
-  socklen_t             m_addrLen = sizeof(m_addr);
-	std::string					   m_name;
-  char m_ip[INET_ADDRSTRLEN];
-  int m_port;
+	Hyprutils::OS::CFileDescriptor						m_sockfd;
+	sockaddr_in											m_addr;
+	socklen_t											m_addrLen = sizeof(m_addr);
+	std::string											m_name;
+	char												m_ip[INET_ADDRSTRLEN];
+	int													m_port;
 
-	void						   recvLoop();
-	bool						   registerSession();
+	void recvLoop();
+	bool registerSession();
 
-	void						   onConnect();
-	void						   onDisconnect();
-	void						   onErrno(eEventType);
-	void						   onRecv(const SRecvData &data);
-	void						   onSend(const std::string &msg);
-	void						   onShutdown();
+	void onConnect();
+	void onDisconnect();
+	void onErrno(eEventType);
+	void onRecv(const SRecvData &data);
+	void onSend(const std::string &msg);
+	void onShutdown();
 
 	friend class CSessionManager;
 };
