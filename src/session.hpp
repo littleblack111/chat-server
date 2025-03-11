711501e617e7a8c8f87d33c27be6faee0997801e
littleblack111 <littleblack11111@gmail.com>
2025-03-11 19:09:47 +0800

    session: get/use IP and port on connect

--- src/session.hpp
+++ src/session.hpp
@@ -3,12 +3,13 @@
 #include "FileDescriptor.hpp"
 #include "format.hpp"
 #include <memory>
+#include <netinet/in.h>
 #include <string>
 #include <thread>
 
 class CSession {
   public:
-	CSession(Hyprutils::OS::CFileDescriptor m_sockfd);
+	CSession();
 	~CSession();
 
 	struct SRecvData {
@@ -19,30 +20,42 @@ class CSession {
 	};
 
 	std::string getName();
-	bool		isValid();
+	void setSelf(std::pair<std::jthread, std::shared_ptr<CSession>> *self);
+
 	SRecvData	read();
 	SRecvData	read(const std::string &msg);
 	bool		write(const std::string &msg, eFormatType type);
 	template <typename... Args>
 	bool write(eFormatType type, std::format_string<Args...> fmt, Args &&...args);
+
 	void run();
-	void setSelf(std::pair<std::jthread, std::shared_ptr<CSession>> *self);
+
+	bool		isValid();
+
 
   private:
-	std::pair<std::jthread, std::shared_ptr<CSession>> *self;
 	enum eEventType : std::uint8_t {
 		READ,
 		WRITE
 	};
+
+	std::pair<std::jthread, std::shared_ptr<CSession>> *self;
 	Hyprutils::OS::CFileDescriptor m_sockfd;
+  sockaddr_in           m_addr;
+  socklen_t             m_addrLen = sizeof(m_addr);
 	std::string					   m_name;
+  char m_ip[INET_ADDRSTRLEN];
+  int m_port;
+
 	void						   recvLoop();
 	bool						   registerSession();
+
 	void						   onConnect();
 	void						   onDisconnect();
 	void						   onErrno(eEventType);
 	void						   onRecv(const SRecvData &data);
 	void						   onSend(const std::string &msg);
 	void						   onShutdown();
+
 	friend class CSessionManager;
 };
