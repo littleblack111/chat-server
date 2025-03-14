c66fb89392f7332e6da7dfab01d8d0acfd7a9eb1
littleblack111 <littleblack11111@gmail.com>
2025-03-14 11:00:38 +0800

    refactor(log): removed redundant overloads

--- src/log.hpp
+++ src/log.hpp
@@ -3,21 +3,22 @@
 #include <format>
 #include <print>
 #include <string>
+#include <unistd.h>
 
 inline void log(FILE *_stream, eFormatType type, std::string str) {
 	std::println(_stream, "{}", NFormatter::fmt(type, str));
 }
 
-inline void log(eFormatType type, std::string str) {
-	log(stdout, type, str);
-}
-
 template <typename... Args>
 void log(FILE *_stream, eFormatType type, std::format_string<Args...> fmt, Args &&...args) {
-	log(_stream, type, std::format(fmt, std::forward<Args>(args)...));
+	std::println(_stream, "{}", NFormatter::fmt(type, fmt, std::forward<Args>(args)...));
+}
+
+inline void log(eFormatType type, std::string str) {
+	log(type == ERR ? stderr : stdout, type, str);
 }
 
 template <typename... Args>
 void log(eFormatType type, std::format_string<Args...> fmt, Args &&...args) {
-	log(stdout, type, std::format(fmt, std::forward<Args>(args)...));
+	log(type == ERR ? stderr : stdout, type, fmt, std::forward<Args>(args)...);
 }
