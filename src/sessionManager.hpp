454c686beb412e28b243af8c00b4b78aa7fc8d97
littleblack111 <littleblack11111@gmail.com>
2025-03-17 00:19:35 +0800

    feat: kick client (#6)

--- src/sessionManager.hpp
+++ src/sessionManager.hpp
@@ -15,12 +15,15 @@ class CSessionManager {
 
 	void run();
 	void broadcast(const std::string &msg) const;
-	void kick(const CSession *session) const;
+	void kick(const CSession *session, const std::string &reason = "") const;
 	void addSession(const CSession &session);
-	void removeSession(std::pair<std::jthread, std::shared_ptr<CSession>> *session);
+	void removeSession(std::pair<std::jthread, std::shared_ptr<CSession>> *session, const bool &kill = false);
 
 	bool nameExists(const std::string &name);
 	void shutdownSessions();
+
+	const CSession *getByName(const std::string &name) const;
+	const CSession *getByIp(const char m_ip[INET_ADDRSTRLEN]) const;
 
   private:
 	void broadcastChat(const std::string &msg, const std::string &username) const;
